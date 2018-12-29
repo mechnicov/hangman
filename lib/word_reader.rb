a5a64614131f94dd1fe645c7af5d7826f2c3194a
Edem <keltikan@gmail.com>
2018-12-30 02:53:52 +0300

    User input mode added

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -1,3 +1,4 @@
+require 'io/console'
 class WordReader
   def read_from_file(path)
     return nil unless File.exist?(path)
@@ -6,5 +7,10 @@ class WordReader
     return nil if lines.empty?
 
     lines.sample
+  end
+
+  def read_from_console
+    STDOUT.print 'Загадывайте слово (буквы не будут видны) > '
+    STDIN.noecho(&:gets).chomp
   end
 end
--- viselica.rb
+++ viselica.rb
@@ -3,11 +3,20 @@ require_relative 'lib/result_printer'
 require_relative 'lib/word_reader'
 
 current_path = __dir__
+game_mode    = nil
+until %w(к п).include? game_mode
+  STDOUT.puts "Кто будет загадывать слово?\n(К)омпьютер\n(П)ользователь"
+  game_mode = gets.chomp.downcase
+end
 
 reader = WordReader.new
-word = reader.read_from_file(current_path + '/data/words.txt')
-game = Game.new(word)
+word   = if game_mode == 'к'
+           reader.read_from_file(current_path + '/data/words.txt')
+         else
+           reader.read_from_console
+         end
 
+game    = Game.new(word)
 printer = ResultPrinter.new(game)
 printer.cls
 printer.intro
